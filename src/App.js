082129c8a26948f1225f5f8d1c2a51bfb996f13f
Alan Coiradas <coiradasalan@gmail.com>
2023-06-21 11:23:23 -0300

    Se modificó el tamaño de los botones y se añadió el logo de freeCodeCamp a la app con sus estilos css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import './components/Row'
-import './components/Button'
+import './components/Row';
+import './components/Button';
 import Row from './components/Row';
+import freeCodeCampLogo from './images/freecodecamp-logo.png';
 function App() {
   const calculator = [[1, 2, 3, '+'], [4, 5, 6, '-'], [7, 8, 9, '*'], ['=', 0, '.', '/']]
   const createButtons = (arr) => {
@@ -13,6 +14,12 @@ function App() {
     }
   return (
     <div className="App">
+      <div className='logo-container'>
+        <img 
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp' />
+      </div>
       <div className='appContainer'>
         {createButtons(calculator)}
       </div>
